f61960aeeb3a143378ea57adf4b6346e84e73626
KiteCrown <162701747+KiteCrown@users.noreply.github.com>
2025-06-15 16:03:56 +0800

    complete boss and add sfx

--- main.ts
+++ main.ts
@@ -9,6 +9,8 @@ sprites.onOverlap(SpriteKind.Player, SpriteKind.boss, function (sprite, otherSpr
     if (sprite.bottom < otherSprite.y && sprite.vy > 0) {
         mySprite.vy = -80
         statusbar.value += -10
+        extraEffects.createSpreadEffectOnAnchor(otherSprite, extraEffects.createSingleColorSpreadEffectData(1, ExtraEffectPresetShape.Spark), 100)
+        music.play(music.createSoundEffect(WaveShape.Square, 200, 1, 255, 0, 100, SoundExpressionEffect.None, InterpolationCurve.Curve), music.PlaybackMode.UntilDone)
     } else {
         beDamaged(true)
     }
@@ -21,33 +23,6 @@ controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
 })
 scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile4`, function (sprite, location) {
     beDamaged(false)
-})
-controller.down.onEvent(ControllerButtonEvent.Released, function () {
-    if (face == 1) {
-        controller.moveSprite(mySprite, 100, 0)
-        mySprite.setImage(img`
-            . 2 2 2 . . . . 
-            . 2 b b b b . . 
-            . 2 b b b b . . 
-            2 2 b f f f . . 
-            . . b b f b . . 
-            . . 2 2 2 2 . . 
-            . . c c c 2 . . 
-            . . c . . c . . 
-            `)
-    } else {
-        controller.moveSprite(mySprite, 100, 0)
-        mySprite.setImage(img`
-            . . . . 2 2 2 . 
-            . . b b b b 2 . 
-            . . b b b b 2 . 
-            . . f f f b 2 2 
-            . . b f b b . . 
-            . . 2 2 2 2 . . 
-            . . 2 c c c . . 
-            . . c . . c . . 
-            `)
-    }
 })
 controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
     face = 0
@@ -115,6 +90,7 @@ function beDamaged (bool: boolean) {
     scene.cameraShake(4, 500)
     info.changeLifeBy(-1)
     mySprite.startEffect(effects.ashes, 1000)
+    music.play(music.createSoundEffect(WaveShape.Noise, 914, 914, 255, 0, 500, SoundExpressionEffect.None, InterpolationCurve.Curve), music.PlaybackMode.UntilDone)
     if (info.life() < 4) {
         color.startFadeFromCurrent(color.GrayScale, 500)
     }
@@ -125,7 +101,7 @@ function beDamaged (bool: boolean) {
     }
 }
 statusbars.onZero(StatusBarKind.EnemyHealth, function (status) {
-    BOSS_fight_start = 0
+    boss_fight_start = 0
     boss1.setImage(img`
         . . . . 2 2 f 2 2 f . . . . . . 
         . . . . f f 2 2 2 2 f f . . . . 
@@ -144,6 +120,110 @@ statusbars.onZero(StatusBarKind.EnemyHealth, function (status) {
         . . . . . . f f f f f f f f . . 
         . . . . . . . . f f f f f . . . 
         `)
+    boss1.vx = 0
+    boss1.setFlag(SpriteFlag.GhostThroughSprites, true)
+    animation.stopAnimation(animation.AnimationTypes.All, boss1)
+    animation.runImageAnimation(
+    boss1,
+    [img`
+        . 2 . . f 2 f f . . . . . . . . 
+        . . 2 f 2 f 2 2 f f . . . . . . 
+        . f b 2 2 2 f 2 2 b f . . . . . 
+        . f 1 1 1 1 1 1 1 d f . . . . . 
+        f d 1 1 1 1 1 1 1 d d f . . . . 
+        f d 1 1 1 1 1 1 d d d f . . . . 
+        f d 1 1 1 d d d d d d f . . . . 
+        f d 1 d f b d d d d b f . . . . 
+        f b d d f c d b b b c f . . . . 
+        . f 1 1 1 1 1 b b c f . . . . . 
+        . f 1 b 1 f f f f f . . . . . . 
+        . f b f c 1 1 1 b f . . . . . . 
+        . . f f 1 b 1 b f f . . . . . . 
+        . . . f b f b f f f . f . . . . 
+        . . . . f f f f f f f f . . . . 
+        . . . . . . f f f f f . . . . . 
+        `,img`
+        . 2 . . f 2 f f . . . . . . . . 
+        . . 2 f 2 f 2 2 f f . . . . . . 
+        . f b 2 2 2 f 2 2 b f . . . . . 
+        . f 1 1 1 1 1 f 1 d f . . . . . 
+        f d 1 1 1 1 1 1 1 d d f . . . . 
+        f d 1 1 1 1 1 1 d d d f . . . . 
+        f d 1 1 1 d d d d d d f . . . . 
+        f d 1 d f b d d d d b f . . . . 
+        f b d d f c d b b b c f . . . . 
+        . f 1 1 1 1 1 b b c f . . . . . 
+        . f 1 b 1 f f f f f . . . . . . 
+        . f b f c 1 1 1 b f . . . . . . 
+        . . f f 1 b 1 b f f . . . . . . 
+        . . . f b f b f f f . f . . . . 
+        . . . . f f f f f f f f . . . . 
+        . . . . . . f f f f f . . . . . 
+        `,img`
+        . 2 . . f 2 f f . . . . . . . . 
+        . . 2 f 2 f 2 2 f f . . . . . . 
+        . f b 2 2 2 f 2 2 b f . . . . . 
+        . f 1 1 1 1 1 f 1 d f . . . . . 
+        f d 1 1 1 1 f 1 1 d d f . . . . 
+        f d 1 1 1 f 1 1 d d d f . . . . 
+        f d 1 1 1 d d d d d d f . . . . 
+        f d 1 d f b d d d d b f . . . . 
+        f b d d f c d b b b c f . . . . 
+        . f 1 1 1 1 1 b b c f . . . . . 
+        . f 1 b 1 f f f f f . . . . . . 
+        . f b f c 1 1 1 b f . . . . . . 
+        . . f f 1 b 1 b f f . . . . . . 
+        . . . f b f b f f f . f . . . . 
+        . . . . f f f f f f f f . . . . 
+        . . . . . . f f f f f . . . . . 
+        `,img`
+        . 2 . . f 2 f f . . . . . . . . 
+        . . 2 f 2 f 2 2 f f . . . . . . 
+        . f b 2 2 2 f 2 2 b f . . . . . 
+        . f 1 1 1 1 1 f 1 d f . . . . . 
+        f d 1 1 1 1 f 1 1 d d f . . . . 
+        f d 1 1 1 f 1 1 d d d f . . . . 
+        f d 1 1 f d d d d d d f . . . . 
+        f d 1 d f f d d d d b f . . . . 
+        f b d d f c f b b b c f . . . . 
+        . f 1 1 1 1 1 b b c f . . . . . 
+        . f 1 b 1 f f f f f . . . . . . 
+        . f b f c 1 1 1 b f . . . . . . 
+        . . f f 1 b 1 b f f . . . . . . 
+        . . . f b f b f f f . f . . . . 
+        . . . . f f f f f f f f . . . . 
+        . . . . . . f f f f f . . . . . 
+        `,img`
+        . 2 . . f 2 f f . . . . . . . . 
+        . . 2 f 2 f 2 2 f f . . . . . . 
+        . f b 2 2 2 f 2 2 b f . . . . . 
+        . f 1 1 1 1 1 f 1 d f . . . . . 
+        f d 1 1 1 1 f 1 1 d d f . . . . 
+        f d 1 1 1 f 1 1 d d d f . . . . 
+        f d 1 1 f d d d d d d f . . . . 
+        f d 1 d f f d d d d b f . . . . 
+        f b d d f c f b b b c f . . . . 
+        . f 1 1 1 1 1 f b c f . . . . . 
+        . f 1 b 1 f f f f f . . . . . . 
+        . f b f c 1 1 1 f f . . . . . . 
+        . . f f 1 b 1 b f f . . . . . . 
+        . . . f b f b f f f . f . . . . 
+        . . . . f f f f f f f f . . . . 
+        . . . . . . f f f f f . . . . . 
+        `],
+    1000,
+    false
+    )
+    timer.after(5000, function () {
+        extraEffects.createSpreadEffectOnAnchor(boss1, extraEffects.createSingleColorSpreadEffectData(10, ExtraEffectPresetShape.Explosion), 5000, 60)
+        extraEffects.createSpreadEffectOnAnchor(boss1, extraEffects.createSingleColorSpreadEffectData(8, ExtraEffectPresetShape.Explosion), 5000, 60)
+        extraEffects.createSpreadEffectOnAnchor(boss1, extraEffects.createSingleColorSpreadEffectData(9, ExtraEffectPresetShape.Explosion), 5000, 60)
+        scene.cameraShake(4, 5000)
+        sprites.destroy(boss1)
+    })
+    timer.after(6000, function () {
+        game.gameOver(true)
+    })
 })
 controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
     face = 1
@@ -258,33 +338,6 @@ sprites.onOverlap(SpriteKind.Player, SpriteKind.Projectile, function (sprite, ot
     } else {
         beDamaged(true)
         otherSprite.destroy()
-    }
-})
-controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
-    if (face == 1) {
-        controller.moveSprite(mySprite, 0, 0)
-        mySprite.setImage(img`
-            . . . . . . . . 
-            . . . . . . . . 
-            . . . . . . . . 
-            . 2 2 2 . . . . 
-            . 2 b b b b . . 
-            2 2 b f f f . . 
-            . . 2 2 2 2 . . 
-            . . c . . c . . 
-            `)
-    } else {
-        controller.moveSprite(mySprite, 0, 0)
-        mySprite.setImage(img`
-            . . . . . . . . 
-            . . . . . . . . 
-            . . . . . . . . 
-            . . . . 2 2 2 . 
-            . . b b b b 2 . 
-            . . f f f b 2 2 
-            . . 2 2 2 2 . . 
-            . . c . . c . . 
-            `)
     }
 })
 sprites.onOverlap(SpriteKind.Player, SpriteKind.shootenemy, function (sprite, otherSprite) {
@@ -569,7 +622,7 @@ scene.onOverlapTile(SpriteKind.Player, assets.tile`tile49`, function (sprite, lo
         boss1.sayText("T.H.E.N  L.E.T  T.H.E  F.I.G.H.T  B.E.G.I.N.", 5000, true)
     })
     timer.after(15000, function () {
-        BOSS_fight_start = 1
+        boss_fight_start = 1
         boss1.vx = 40
         statusbar = statusbars.create(50, 4, StatusBarKind.EnemyHealth)
         statusbar.setPosition(119, 9)
@@ -725,6 +778,7 @@ sprites.onOverlap(SpriteKind.Player, SpriteKind.flyer, function (sprite, otherSp
 function JumpAction () {
     if (mySprite.isHittingTile(CollisionDirection.Bottom)) {
         mySprite.vy = -150
+        music.play(music.createSoundEffect(WaveShape.Square, 400, 600, 255, 255, 100, SoundExpressionEffect.None, InterpolationCurve.Linear), music.PlaybackMode.UntilDone)
     }
 }
 sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
@@ -822,6 +876,7 @@ let projectile3: Sprite = null
 let heart: Sprite = null
 let death_animater: Sprite = null
 let boss1: Sprite = null
+let boss_fight_start = 0
 let statusbar: StatusBarSprite = null
 let bat: Sprite = null
 let enemy2: Sprite = null
@@ -829,7 +884,6 @@ let enemy1: Sprite = null
 let mySprite: Sprite = null
 let face = 0
 let BOSS_fight_start = 0
-BOSS_fight_start = 0
 let moneynum = 0
 info.setLife(10)
 profilelife.setMaxLife(10)
@@ -1270,7 +1324,7 @@ game.onUpdate(function () {
     }
 })
 game.onUpdate(function () {
-    if (BOSS_fight_start == 1) {
+    if (boss_fight_start == 1) {
         if (boss1.isHittingTile(CollisionDirection.Left)) {
             boss1.vx = 40
             animation.runImageAnimation(
@@ -1359,7 +1413,7 @@ game.onUpdate(function () {
     }
 })
 game.onUpdateInterval(700, function () {
-    if (BOSS_fight_start == 1) {
+    if (boss_fight_start == 1) {
         if (boss1.vx == 40) {
             projectile3 = sprites.createProjectileFromSprite(img`
                 . . . . . . . . . . . . . . . . 
@@ -1504,12 +1558,9 @@ forever(function () {
     music.play(music.createSong(hex`0078000408040600001c00010a006400f401640000040000000000000000000000000005000004d80000000400011904000800019c0c001000011910001400019c18001c00011e1c001e00011e1e002000011e20002400012024002800019c28002c00011b2c003000011930003200012032003400012034003600019c36003800019c38003a00011b3a003c00011b3c003e0001193e004000011940004400011944004800019c4c004e0001194e005000011950005400019c58005c00011e5e006000011e60006400012068006c00019c7000720001a17200740001a17400760001a17600780001a178007a0001207a007c0001207c007e0001207e008000012003001c0001dc00690000045e0100040000000000000000000005640001040003300020002800011928003000011830003800019738004000011660006800011968007000011870007800019778008000011805001c000f0a006400f4010a0000040000000000000000000000000000000002c00000000400011904000800011b08000c00019c0c001000011e10001400011914001800011b18001c00019c1c002000011e20002400012024002800019c28002c00011b2c003000011930003400012034003800019c38003c00011b3c004000011940004400011944004800011b48004c00019c4c005000011e50005400011954005800011b58005c00019c5c006000011e60006400012064006800019c68006c00011b6c007000011970007400012074007800019c78007c00011b7c008000011907001c00020a006400f4016400000400000000000000000000000000000000031c0000000400021925100014000297a320002400029ca830003400029eaa08001c000e050046006603320000040a002d00000064001400013200020100020c0000001000010d40005000010d09010e02026400000403780000040a000301000000640001c80000040100000000640001640000040100000000fa0004af00000401c80000040a00019600000414000501006400140005010000002c0104dc00000401fa0000040a0001c8000004140005d0076400140005d0070000c800029001f40105c201f4010a0005900114001400039001000005c201f4010500058403050032000584030000fa00049001000005c201f4010500058403c80032000584030500640005840300009001049001000005c201f4010500058403c80064000584030500c8000584030000f40105ac0d000404a00f00000a0004ac0d2003010004a00f0000280004ac0d9001010004a00f0000280002d00700040408070f0064000408070000c80003c800c8000e7d00c80019000e64000f0032000e78000000fa00032c01c8000ee100c80019000ec8000f0032000edc000000fa0003f401c8000ea901c80019000e90010f0032000ea4010000fa0001c8000004014b000000c800012c01000401c8000000c8000190010004012c010000c80002c800000404c8000f0064000496000000c80002c2010004045e010f006400042c010000640002c409000404c4096400960004f6090000f40102b80b000404b80b64002c0104f40b0000f401022003000004200300040a000420030000ea01029001000004900100040a000490010000900102d007000410d0076400960010d0070000c800a5000000010001000800090001001000110001001800190001001c001d000100200021000200062800290002000630003100020006380039000200063c003d00010040004100050003060708480049000200084e004f000106500051000100540055000106580059000200085c005d00010060006100020008640065000106680069000200086c006d00010670007100020008740075000106780079000200087c007d00020006`), music.PlaybackMode.UntilDone)
     music.play(music.createSong(hex`0078000408040500001c00010a006400f401640000040000000000000000000000000005000004d80000000400011904000800019c0c001000011910001400019c18001c00011e1c001e00011e1e002000011e20002400012024002800019c28002c00011b2c003000011930003200012032003400012034003600019c36003800019c38003a00011b3a003c00011b3c003e0001193e004000011940004400011944004800019c4c004e0001194e005000011950005400019c58005c00011e5e006000011e60006400012068006c00019c7000720001a17200740001a17400760001a17600780001a178007a0001207a007c0001207c007e0001207e008000012003001c0001dc00690000045e0100040000000000000000000005640001040003300020002800011928003000011b30003800019c38004000011e60006800012068007000019c7000780001a178008000012005001c000f0a006400f4010a0000040000000000000000000000000000000002c00000000400011904000800011b08000c00019c0c001000011e10001400011914001800011b18001c00019c1c002000011e20002400012024002800019c28002c00011b2c003000011930003400012034003800019c38003c00011b3c004000011940004400011944004800011b48004c00019c4c005000011e50005400011954005800011b58005c00019c5c006000011e60006400012064006800019c68006c00011b6c007000011970007400012074007800019c78007c00011b7c008000011908001c000e050046006603320000040a002d0000006400140001320002010002120000001000018b40005000018960007000010809010e02026400000403780000040a000301000000640001c80000040100000000640001640000040100000000fa0004af00000401c80000040a00019600000414000501006400140005010000002c0104dc00000401fa0000040a0001c8000004140005d0076400140005d0070000c800029001f40105c201f4010a0005900114001400039001000005c201f4010500058403050032000584030000fa00049001000005c201f4010500058403c80032000584030500640005840300009001049001000005c201f4010500058403c80064000584030500c8000584030000f40105ac0d000404a00f00000a0004ac0d2003010004a00f0000280004ac0d9001010004a00f0000280002d00700040408070f0064000408070000c80003c800c8000e7d00c80019000e64000f0032000e78000000fa00032c01c8000ee100c80019000ec8000f0032000edc000000fa0003f401c8000ea901c80019000e90010f0032000ea4010000fa0001c8000004014b000000c800012c01000401c8000000c8000190010004012c010000c80002c800000404c8000f0064000496000000c80002c2010004045e010f006400042c010000640002c409000404c4096400960004f6090000f40102b80b000404b80b64002c0104f40b0000f401022003000004200300040a000420030000ea01029001000004900100040a000490010000900102d007000410d0076400960010d0070000c800cb0000000100020006080009000200080e000f000106100011000100140015000106180019000200081c001d000200061e001f00010620002100020008240025000106280029000200082c002d00010630003100020008340035000106380039000200083c003d0002000640004100020006480049000200084e004f000106500051000100540055000106580059000200085c005d00010060006100020008640065000106680069000200086c006d00010670007100020008740075000106780079000200087c007d00020006`), music.PlaybackMode.UntilDone)
 })
-forever(function () {
-	
-})
 game.onUpdateInterval(3000, function () {
-    if (BOSS_fight_start == 1) {
-        boss1.sayText("head bash!", 1000, true)
+    if (boss_fight_start == 1) {
+        extraEffects.createSpreadEffectOnAnchor(boss1, extraEffects.createSingleColorSpreadEffectData(5, ExtraEffectPresetShape.Twinkle), 100)
         timer.after(1000, function () {
             boss1.vy = -170
         })
